5175d6417fc113d490860d2685a4c0097a948d27
jansule <j_sule02@wwu.de>
2018-05-17 23:51:46 +0200

    Add Material-Ui
      - add Roboto font
      - add buttons (not doing anything yet)
      - add image placeholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 import './App.css';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 class App extends Component {
+  
   render() {
+
+    let buttonStyle = {
+      margin: 12,
+    };
+
+    let imgStyle = {
+      width: 500,
+      height: 500,
+      "background-color": "grey",
+      "margin-top": 12,
+    };
+
+    let imgPath = "";
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to satellite-background</h1>
         </header>
-        <p className="App-intro">
-          Hello Universe!
-        </p>
+        <MuiThemeProvider layout="column">
+          <img src={imgPath} alt="satellite-image" style={imgStyle}/>
+          <div layout="row">
+            <RaisedButton label="Load new Image" style={buttonStyle} />
+            <RaisedButton label="Change Background" primary={true} style={buttonStyle} />
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
